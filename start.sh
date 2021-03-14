02f3047cbe4f73ec10a15b885d006a8725b925fd
groot <melhin444@gmail.com>
2021-03-14 19:03:32 +0100

    Changing workers to 1 for free tier

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ echo "Downloading models..."
 python download_language_models.py --source=de --target=en
 
 echo "Starting image renderer server..."
-uvicorn main:app --host 0.0.0.0 --port $PORT --workers=2
+uvicorn main:app --host 0.0.0.0 --port $PORT --workers=1
