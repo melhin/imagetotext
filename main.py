dbdf9bbf1d194edc233019cf914b2aa77083950b
melhin <melhin444@gmail.com>
2021-08-22 18:52:08 +0200

    Formatting before splitting

--- main.py
+++ main.py
@@ -8,17 +8,18 @@ from fastapi import FastAPI, File
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from starlette.requests import Request
+from starlette.responses import Response
 
 from translator import Translator
 
-TESSERACT_PATH = '/usr/bin/tesseract'
-LANGUAGE_MODEL_DIR = os.path.join(os.getcwd(), 'data')
+TESSERACT_PATH = "/usr/bin/tesseract"
+LANGUAGE_MODEL_DIR = os.path.join(os.getcwd(), "data")
 
 
 def read_img(img):
     pytesseract.pytesseract.tesseract_cmd = TESSERACT_PATH
     text = pytesseract.image_to_string(img)
-    return(text)
+    return text
 
 
 translator = Translator(LANGUAGE_MODEL_DIR)
@@ -27,6 +28,7 @@ app = FastAPI()
 
 class Translate(BaseModel):
     text: str
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -37,23 +39,23 @@ app.add_middleware(
 )
 
 
-@app.get('/ping/')
+@app.get("/ping/")
 def ping(request: Request):
     return True
 
 
-@app.post('/predict/')
+@app.post("/predict/")
 def prediction(request: Request, file: bytes = File(...)):
-    if request.method == 'POST':
+    if request.method == "POST":
         image_stream = io.BytesIO(file)
         image_stream.seek(0)
         file_bytes = np.asarray(bytearray(image_stream.read()), dtype=np.uint8)
         frame = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
         label = read_img(frame)
         return label
-    return 'No post request found'
+    return Response(status_code=400)
 
 
-@app.post('/translate/')
+@app.post("/translate/")
 def translation(translate: Translate):
-    return translator.translate('de', 'en', translate.text)
+    return translator.translate("de", "en", translate.text)
