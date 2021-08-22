7c517dc5118e96575ed832ca444fc89571c82641
melhin <melhin444@gmail.com>
2021-08-22 19:02:06 +0200

    Removing translation functionality

--- data/Instructions.md
+++
@@ -1,2 +0,0 @@
-This is a placeholder for having model files here. Please
-use the download_language_models.py to download required models 
--- start.sh
+++ start.sh
@@ -2,8 +2,5 @@
 
 set -e
 
-echo "Downloading models..."
-python download_language_models.py --source=de --target=en
-
 echo "Starting image renderer server..."
 uvicorn main:app --host 0.0.0.0 --port $PORT --workers=1
